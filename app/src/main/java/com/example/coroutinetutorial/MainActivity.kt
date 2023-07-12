2b2335abd4da197672f4a951da1d63cd7d4b9127
Leraedon <thor0249@gmail.com>
2023-07-12 16:51:20 +0900

    ytb

--- app/src/main/java/com/example/coroutinetutorial/MainActivity.kt
+++ app/src/main/java/com/example/coroutinetutorial/MainActivity.kt
@@ -4,11 +4,10 @@ import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -25,9 +24,47 @@ class MainActivity : AppCompatActivity() {
 
         Log.d(TAG, "onCreate: ${Thread.currentThread().name}")*/
 
-        lifecycleScope.launch {
+        /*lifecycleScope.launch {
             myFunc()
+        }*/
+
+        /*val job = lifecycleScope.launch(Dispatchers.IO) {
+            repeat(5) {
+                Log.d("Leraedon", "코루틴 !!!")
+                delay(1000)
+            }
         }
+
+        lifecycleScope.launch {
+            delay(2000)
+            job.cancel()
+            Log.d("Leraedon", "코루틴 취소")
+        }*/
+
+        /*repeat(50) {
+            lifecycleScope.launch(Dispatchers.Default) {
+                Log.d("Leraedon", Thread.currentThread().name)
+            }
+        }*/
+
+        lifecycleScope.launch(Dispatchers.IO){
+            val time = measureTimeMillis {
+                val result1 = async { network1() }
+                val result2 = async { network2() }
+                Log.d("Leraedon", "결과 1: ${result1.await()}")
+                Log.d("Leraedon", "결과 2: ${result2.await()}")
+            }
+            Log.d("Leraedon", "시간: $time")
+        }
+    }
+
+    suspend fun network1(): String {
+        delay(2000)
+        return "result 1"
+    }
+    suspend fun network2(): String {
+        delay(2000)
+        return "result 2"
     }
 
     suspend fun myFunc(): String {
